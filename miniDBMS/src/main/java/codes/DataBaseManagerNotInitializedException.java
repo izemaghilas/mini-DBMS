bf1a75b490107746ea93d2033a273668fc98c59e
izemaghilas <saidtala23@gmail.com>
2021-02-17 13:56:11 +0100

    define exception if databasemanager not set

---
+++ miniDBMS/src/main/java/codes/DataBaseManagerNotInitializedException.java
@@ -0,0 +1,13 @@
+package codes;
+
+public class DataBaseManagerNotInitializedException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6718144951522223996L;
+	
+	public DataBaseManagerNotInitializedException() {
+		super("DataBaseManager must be initialized");
+	}
+}
