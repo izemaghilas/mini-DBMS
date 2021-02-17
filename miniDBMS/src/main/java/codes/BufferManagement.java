ed2b8540a95fe5e2b286f422c9377170e72aad94
izemaghilas <saidtala23@gmail.com>
2021-02-17 13:54:27 +0100

    define methods to set pool and replacement policy

--- miniDBMS/src/main/java/codes/BufferManagement.java
+++ miniDBMS/src/main/java/codes/BufferManagement.java
@@ -4,17 +4,19 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import core.buffer_management_layer.PageIdentifier;
-import core.buffer_management_layer.Pool;
-import core.buffer_management_layer.ReplacementPolicy;
 
 public interface BufferManagement {
 	
-	public void init(Pool bufferPool, ReplacementPolicy replacementPolicy);
+	public void init();
 	
 	public void reset();
 	
 	public byte[] getPage(PageIdentifier pageId, DiskSpaceManagement diskManager) throws FileNotFoundException, IOException;
 	
 	public void freePage(PageIdentifier pageId, boolean isDirty);
+	
+	public void setBufferPool(Object bufferPool);
+	
+	public void setReplacementPolicy(Object replacementPolicy);
 	
 }
